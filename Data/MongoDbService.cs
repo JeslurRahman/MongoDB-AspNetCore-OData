74d8c467e18ec245bb51f9f6758ed9ce3dabd490
Jeslur Rahman <88666503+it20667696@users.noreply.github.com>
2024-06-30 03:17:33 +0530

    implement aspnetcore application with mongodb

---
+++ Data/MongoDbService.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDB_OData.Data
+{
+    public class MongoDbService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase _database;
+        public MongoDbService(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+
+            var connectionString = _configuration.GetConnectionString("DbConnection");
+            var databaseName = _configuration.GetSection("MongoDbSettings:DatabaseName").Value;
+
+            var mongoUrl = MongoUrl.Create(connectionString);
+            var mongoClient = new MongoClient(mongoUrl);
+
+            _database = mongoClient.GetDatabase(databaseName);
+        }
+
+        public IMongoDatabase? Database => _database;
+    }
+}
